e8fb651a112c0ecfa351b4803b96801069b70ec2
admin2 <admin2@cn18s-mbp.home>
2022-07-26 16:52:10 +0100

    feat: static server working

---
+++ src/server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app =  express()
+const port = process.env.PORT || 5001;
+
+
+app.use("/static", express.static("public"));
+//access public folder when a req is made to static
+
+app.listen(port, ()=> {
+    console.log(`Listing on port ${port}`)
+})
+
+``
